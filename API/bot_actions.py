b4558a38f612052bbdc938eba4de3c5b498ce849
Sepehrmasihpour <sepehr.masihpour1381@gmail.com>
2024-04-24 11:41:35 +0330

    I fixed the caption tests
    The test that were for caption were not working. I realized the problem was that in the expected_result functin I put the expected_result variable in outsiede of the loop so any time that I would change it it would be rewritten insted being made a new for evry iteration. I simply put it back in the loop and vala. I also updated the documentation in the bot_action module

--- API/bot_actions.py
+++ API/bot_actions.py
@@ -2,7 +2,7 @@ from typing import IO
 from data import telegram_ids
 import httpx
 
-
+# Extracting the BOT_TOKEN from the data module.
 BOT_TOKEN = telegram_ids["BOT_TOKEN"]
 
 
@@ -12,9 +12,39 @@ def send_media_via_bot(
     chat_id: str | int,
     caption: str | None = None,
 ):
+    """
+    Sends media (photo, audio, video) or text message via Telegram bot.
 
-    media_types = ["photo", "text", "audio", "video"]  # Supported media types.
+    Args:
+        media (str or IO[any]): The media content to be sent. It can be a file_id,url(str) or an opened file (IO[any]).
+        media_type (str): The type of media to be sent. It should be one of: 'photo', 'audio', 'video', 'text'.
+        chat_id (str or int): The ID of the Telegram chat where the media will be sent.
+        caption (str, optional): Caption for the media (only applicable for photo, audio, video). Defaults to None.
+
+    Returns:
+        dict: A dictionary containing the response from the Telegram API.
+              If the request is successful, the response will contain the sent message data.
+              If an error occurs, the response will contain an 'error' key with a description of the error.
+
+    Raises:
+        None
+
+    Note:
+        This function uses the httpx library to make HTTP requests to the Telegram Bot API.
+
+    Example:
+        # Sending a photo with caption
+        response = send_media_via_bot("path/to/photo.jpg", "photo", 123456, caption="Check this out!")
+        print(response)
+    """
+
+    # List of supported media types
+    media_types = ["photo", "text", "audio", "video"]
+
+    # Converting the input media type to lowercase
     input_media_type = media_type.lower()
+
+    # Checking if the input media type is valid
     if input_media_type in media_types:
         # Constructing the request URL based on the media type.
         general_url = f"https://api.telegram.org/bot{BOT_TOKEN}/send"
@@ -23,9 +53,11 @@ def send_media_via_bot(
             if input_media_type != "text"
             else f"{general_url}Message"
         )
-        try:
 
+        try:
+            # Handling different types of media content
             if type(media) == str:
+                # If media is a file path
                 if caption:
                     response = httpx.post(
                         request_url,
@@ -38,22 +70,29 @@ def send_media_via_bot(
                         params={"chat_id": chat_id},
                         data={input_media_type: media},
                     )
-
             else:
+                # If media is an opened file
                 response = httpx.post(
                     request_url,
                     params={"caption": caption, "chat_id": chat_id},
                     files={input_media_type: media},
                 )
+
+            # Parsing the response JSON
             response_json = response.json()
+
+            # Checking if the request was successful
             if response_json.get("ok"):
                 return response_json
             else:
+                # Returning error response if request failed
                 return {
                     "error": response_json.get("description", "Unknown error occurred")
                 }
         except Exception as e:
+            # Returning error response if an exception occurred during request
             return {"error": str(e)}
 
     else:
+        # Returning error response for unsupported media type
         return {"error": "unacceptable media type"}
--- API/test_main.py
+++ API/test_main.py
@@ -2,6 +2,7 @@
 #! This import dosent work inside the tests/endpoints directory find out why and fix it\
 from data import (
     media_test_cases,
+    uniqe_chat_ids,
     telegram_ids,
 )
 from fastapi.testclient import TestClient
@@ -51,17 +52,17 @@ def expected_media_test_results():
     # Prepare expected results based on test cases
     test_cases = media_test_cases
     expected_results = []
-    expected_result = {
-        "result": {
-            "chat": {
-                "id": "",
-            },
-            "caption": None,
-        },
-    }
 
     # Iterate through test cases to set expected results
     for case in test_cases:
+        expected_result = {
+            "result": {
+                "chat": {
+                    "id": "",
+                },
+                "caption": None,
+            },
+        }
         if case["caption"]:
             expected_result["result"]["caption"] = case["caption"]
 
@@ -94,29 +95,53 @@ def test_send_media_status():
 
 def test_send_media_chat_id():
     # Test to ensure response chat ID matches expected chat ID
-    test_results = media_results
+    results = media_results
     expected_results = media_expected_results
-    for response, expected_response in zip(test_results, expected_results):
-        response_json = response.json()
-        chat_id = str(response_json["result"]["chat"]["id"])
-        expected_chat_id = expected_response["result"]["chat"]["id"]
+    for response, expected_response in zip(results, expected_results):
+        try:
+            response_json = response.json()
+        except AttributeError:
+            print("Error: Response does not support json method")
+            continue
+
+        # Safer access to nested dictionary keys
+        chat_id_info = response_json.get("result", {}).get("chat", {})
+        chat_id = chat_id_info.get("id")
+        if chat_id is None:
+            print("Chat ID not found in the response")
+            continue
+
+        # Convert to string and check conditions
+        chat_id = str(chat_id)
+        # Access expected chat ID safely
+        expected_chat_id_info = expected_response.get("result", {}).get("chat", {})
+        expected_chat_id = expected_chat_id_info.get("id")
+        if expected_chat_id in uniqe_chat_ids:
+            expected_chat_id = uniqe_chat_ids[expected_chat_id]
+        # Assert condition
         assert (
             chat_id == expected_chat_id
-        ), f"The chatID is not as expected.\nchatID:{chat_id}\nexpected chat ID:{expected_chat_id}"
+        ), f"The chat ID is not as expected.\nchat ID: {chat_id}\nexpected chat ID: {expected_chat_id}"
 
 
 def test_send_media_caption():
     # Test to ensure response caption matches expected caption
-    test_results = media_results
+    results = media_results
     expected_results = media_expected_results
-    for response, expected_response in zip(test_results, expected_results):
-        response_json = response.json()
-        expected_caption = expected_response["result"]["caption"]
+    for response, expected_response in zip(results, expected_results):
+        try:
+            response_json = response.json()
+        except AttributeError:
+            print("Error: Response does not support json method")
+            continue
+
+        expected_caption = expected_response.get("result", {}).get("caption")
         caption = response_json.get("result", {}).get("caption", None)
 
-        if expected_caption:
+        if expected_caption is not None:
+            assert caption is not None, f"The caption is missing."
             assert (
-                caption
+                caption == expected_caption
             ), f"The caption is not as expected.\nResponse caption: {caption}\nExpected caption: {expected_caption}"
         else:
             assert caption is None, f"Expected no caption, but got: {caption}"
