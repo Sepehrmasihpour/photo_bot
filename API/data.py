19f9aa772d289d7595dda32dfff7389ad9c1c8cd
Sepehrmasihpour <sepehr.masihpour1381@gmail.com>
2024-05-30 18:43:51 +0330

    I made the test fot offset param
    the offset param test requires there to be to sets update made before and after the middle part of the test. I want to solve this problem with the help of the uset input and that will, I know it is a very iellagant way to solve the issiue. but if it works it works. ufortunatly it doesnt work at the moment. I fix the issure late

--- API/data.py
+++ API/data.py
@@ -10,10 +10,12 @@ telegram_ids = {
     "CHANNEL_ID": os.getenv("TEST_CHANNEL_ID"),
     "GROUP_ID": os.getenv("TEST_GROUP_ID"),
     "BOT_TOKEN": os.getenv("TEST_BOT_TOKEN"),
+    "BOT_CHAT_ID": os.getenv("TEST_BOT_CHAT_ID"),
     # These IDs are for when the app is in production and should be switched out with the real use case
     # "CHANNEL_ID": os.getenv("CHANNEL_ID"),
     # "GROUP_ID ": os.getenv("GROUP_ID"),
     # "BOT_TOKEN": os.getenv("BOT_TOKEN"),
+    # "BOT_CHAT_ID": os.getenv("BOT_CHAT_ID")
     "TEST_USER_BOT_TOKEN": os.getenv(
         "TEST_USER_BOT_TOKEN"
     ),  #! This is a bot that we will use to substitude for a user in regards to tests.
@@ -27,25 +29,25 @@ uniqe_chat_ids = {
 # * This the test data for testing send_media endpoint
 media_test_cases = [
     {
-        "chat_id": "mainGroup",
+        "chat_id": telegram_ids["BOT_CHAT_ID"],
         "media_type": "photo",
         "media": "https://fastly.picsum.photos/id/326/200/200.jpg?hmac=T_9V3kc7xrK46bj8WndwDhPuvpbjnAM3wfL_I7Gu6yA",  # url
         "caption": "Test photo caption",
     },
     {
-        "chat_id": telegram_ids["GROUP_ID"],
+        "chat_id": telegram_ids["BOT_CHAT_ID"],
         "media_type": "photo",
         "media": "AgACAgQAAx0EfU-ioAADsWYB_ebHeQOR1Wg22hYQYMAxwKhSAAJ7wTEbJysQULO2VE-EtnaVAQADAgADcwADNAQ",  # file_id
         "caption": "Test photo caption",
     },
     {
-        "chat_id": "mainGroup",
+        "chat_id": telegram_ids["BOT_CHAT_ID"],
         "media_type": "audio",
         "media": "CQACAgQAAx0EfU-ioAADsmYCBRCwx7kBIWmwrASYWOy06FDhAAKmEwACJysQUEYwlN5ZdJodNAQ",  # file_id
         "caption": "Test audio caption",
     },
     {
-        "chat_id": telegram_ids["GROUP_ID"],
+        "chat_id": telegram_ids["BOT_CHAT_ID"],
         "media_type": "text",
         "media": "testing the sendMessage method",  # file_id
         "caption": None,
--- API/test_main.py
+++ API/test_main.py
@@ -158,7 +158,9 @@ def test_send_media_caption():
 
 
 # the data needed for the getUpdates tests
-def getUpdate_results():
+def getUpdate_results(
+    offset: int | None = None, selected_updates: list[str] | None = None
+):
     """
     Retrieves updates from the Telegram API.
 
@@ -167,7 +169,13 @@ def getUpdate_results():
             Returns None if there's an error fetching updates.
     """
     try:
-        response = client.get("/getUpdates")
+        params = {}
+        if offset is not None:
+            params["offset"] = offset
+        if selected_updates is not None:
+            params["selected_updates"] = selected_updates
+
+        response = client.get("/getUpdates", params=params)
         response.raise_for_status()  # Ensure the request was successful
         return response.json()
     except Exception as e:
@@ -189,30 +197,9 @@ def getUpdate_status():
     return False
 
 
-def is_there_update(updates: list, method_status: bool):
-    """
-    Checks if there are updates available for testing.
-    """
-    updates = getUpdate_response_json
-    updates_result = updates.get("result", [])
-    method_status = getUpdate_status()
-
-    if updates_result == []:
-        return False
-
-    return True
-
-
-def not_enough_updates(updates: list):
-    """
-    Checks if there are enough updates, if not, prompts the user to add updates.
-    """
-    if len(updates) < 3:
-        return False
-    return True
-
-
-def recive_test_updates():
+def recive_test_updates(
+    offset: int | None = None, selected_udpates: list[str] | None = None
+):
     """
     This funciton will send 4 messages of various media types to the test group that the bot can recive updates.
     This funcitn should be used where there are not enough updates to do tests on.
@@ -222,7 +209,7 @@ def recive_test_updates():
     global getUpdate_response_results
 
     test_param = True
-    chat_id_param = telegram_ids["GROUP_ID"]
+    chat_id_param = telegram_ids["BOT_CHAT_ID"]
     media_type_params = ["text", "photo", "audio", "text"]
     media_params = [
         "test_user text message",
@@ -235,18 +222,70 @@ def recive_test_updates():
         send_media_via_bot(
             media=media_params[i],
             media_type=media_type_params[i],
+            caption=caption_params[i],
             chat_id=chat_id_param,
             test=test_param,
         )
+    getUpdate_response_json = getUpdate_results(
+        offset=offset, selected_updates=selected_udpates
+    )
+    getUpdate_response_results = getUpdate_response_json["result"]
+
+
+def get_highest_updateId(maximum: bool = True):
+    update_result = getUpdate_response_results
+    highest_updateId = (
+        max(update["update_id"] for update in update_result)
+        if maximum
+        else min(update["update_id"] for update in update_result)
+    )
+    return highest_updateId
+
+
+def test_getUpdate_status():
+    status = getUpdate_status()
+    assert (
+        status
+    ), f"getUpdate method has a failed called,\ngetUpdate method response : {getUpdate_response_json}"
+
+
+def enough_updates():
+    updates_result = getUpdate_response_results
+    update_result_numbers = updates_result.len()
+    if not updates_result:
+        return False
+    if update_result_numbers < 4:
+        return False
+    return True
+
+
+def ask_user_for_updates():
+    global getUpdate_response_json
+    global getUpdate_response_results
+    print(
+        "please send atleast 4 messages of any kind to the bot\n I will wait for 30 seconds for you, if you don't do this we will set the test as a faillure"
+    )
+    time.sleep(20)
     getUpdate_response_json = getUpdate_results()
     getUpdate_response_results = getUpdate_response_json["result"]
 
 
-def test_getUpdates_status():
-    """
-    Tests the status of the getUpdates method.
-    """
-    response_status = getUpdate_status()
-    assert (
-        response_status == True
-    ), f"Error in the getUpdate response: {getUpdate_response_json}"
+def test_getUpdates_offset():
+    status = getUpdate_status()
+    if not status:
+        assert (
+            status
+        ), "because of the not ok status of the method This test cannot be excuted"
+    enough_updates = enough_updates()
+    if not enough_updates:
+        ask_user_for_updates()
+    enough_updates = enough_updates()
+    if not enough_updates:
+        assert enough_updates, "there are not enough updates for the tests"
+    highest_update_id = get_highest_updateId()
+    ask_user_for_updates()
+    enough_updates = enough_updates()
+    if not enough_updates:
+        assert enough_updates, "there are not enough updates for the tests"
+    lowest_updateId = get_highest_updateId(maximum=False)
+    assert lowest_updateId > highest_update_id, "The offset param is not working"
